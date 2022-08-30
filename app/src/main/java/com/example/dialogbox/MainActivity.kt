5ed2c46ca2621927e1012ebb9f2e997cffed9f1f
Kamal Dhiman <kamal.dhiman2001@gmail.com>
2022-08-30 11:39:16 +0530

    Apply mutlichoice to dialog box

--- app/src/main/java/com/example/dialogbox/MainActivity.kt
+++ app/src/main/java/com/example/dialogbox/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.dialogbox
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
@@ -21,28 +25,51 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     private fun chooseColorDialogBox(){
         val chooseColorAlert = AlertDialog.Builder(this)
         val showColorTextView = findViewById<TextView>(R.id.showColor)
         val colors = arrayOf("RED","BLUE","GREEN","YELLOW")
+        val arrOfSelected = arrayListOf<Int>()
+        var color = SpannableStringBuilder("")
 
         chooseColorAlert.setCancelable(false)
             .setTitle("Choose your favorite color")
-            .setSingleChoiceItems(colors,-1) { _, which->
-                when(which){
-                    0->showColorTextView.setTextColor(Color.RED)
-                    1->showColorTextView.setTextColor(Color.BLUE)
-                    2->showColorTextView.setTextColor(Color.GREEN)
-                    3->showColorTextView.setTextColor(Color.YELLOW)
+            .setMultiChoiceItems(colors,null) { _, which, checked ->
+                if(checked){
+                    arrOfSelected.add(which)
+                } else if(arrOfSelected.contains(which)) {
+                    arrOfSelected.remove(which)
                 }
-                showColorTextView.text = colors[which]
             }
             .setPositiveButton("Ok") { dialog, _ ->
+                for(element in arrOfSelected) {
+                    val color2 = SpannableString(colors[element]+"")
+                    when(element){
+                        0->{
+                            val fColor = ForegroundColorSpan(Color.RED)
+                            color2.setSpan(fColor,0, color2.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+                        }
+                        1->{
+                            val fColor = ForegroundColorSpan(Color.BLUE)
+                            color2.setSpan(fColor,0, color2.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+                        }
+                        2->{
+                            val fColor = ForegroundColorSpan(Color.GREEN)
+                            color2.setSpan(fColor,0, color2.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+                        }
+                        3->{
+                            val fColor = ForegroundColorSpan(Color.YELLOW)
+                            color2.setSpan(fColor,0, color2.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+                        }
+                    }
+                    color.append(color2)
+                }
+                showColorTextView.text = color
                 dialog.dismiss()
             }
             .create()
             .show()
-
     }
 
     override fun onBackPressed() {
